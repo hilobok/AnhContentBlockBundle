9c5fbf621673cac9558eb5dd5a7e77e3de169370
Andrew Hilobok <hilobok@gmail.com>
2014-05-19 15:21:27 +0300

    Initial commit

---
+++ AnhContentBlockBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Anh\ContentBlockBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AnhContentBundle extends Bundle
+{
+    const VERSION = 'v1.0.0';
+    const TITLE = 'AnhContentBlockBundle';
+    const DESCRIPTION = 'Bundle for content blocks management';
+}
